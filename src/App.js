84844550b7c8f525f022be0509be8a0ec38ffa8f
sredifer <redifer.samantha@gmail.com>
2025-03-10 19:33:00 -0700

    Reorganized file structure, matched UI, made different pages * Reorganized pomodoro timer files into the components file within src. * Merged App.js files to have one place where we have all components but can link to different pages * Matched the UI for each page. Could probably be improved if time. * TODO: Page for file upload & friend collab feature. Possible move search feature to another page? TBH

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; // Import routing components
 import Calendar from "./components/calendar/Calendar";
 import SearchBar from "./components/study-library-feature/study-library-feature";
-import resourcesData from "./components/Study-library-resources-data.json" 
+import resourcesData from "./components/Study-library-resources-data.json";
 import SearchBox from "./components/tag-search-bar";
 import optionArray from "./components/constants/options";
+import Timer from "./components/timer/Timer"; // Timer component
+import Settings from "./components/timer/Settings"; // Settings component
+import SettingsContext from "./components/timer/SettingsContext"; // Settings context
+import './App.css';
 
 
 function App() {
+  const [showSettings, setShowSettings] = useState(true);
+  const [workMinutes, setWorkMinutes] = useState(25);
+  const [breakMinutes, setBreakMinutes] = useState(5);
+  const [totalPomodoros, setTotalPomodoros] = useState(4);
+
   const [options, setOptions] = React.useState(optionArray);
 
-  return (
-    <div>
-      <h1>Collaborative Study Organizer</h1>
-      <Calendar />
-      <br />
-      <br />
-      <SearchBar placeholder="Search for a study resource" data={resourcesData}/>
-      <br />
-      <SearchBox options={options} onChange={newOptions => setOptions(newOptions)}/>
 
-    </div>
+  return (
+    <SettingsContext.Provider
+      value={{
+        workMinutes,
+        breakMinutes,
+        totalPomodoros,
+        setWorkMinutes,
+        setBreakMinutes,
+        setTotalPomodoros,
+      }}
+    >
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link> {/* Link to the home page (Timer) */}
+              </li>
+              <li>
+                <Link to="/calendar">Calendar</Link> {/* Link to the calendar page */}
+              </li>
+            </ul>
+          </nav>
+
+
+          <Routes>
+            {/* Route for the calendar + search Bar page) */}
+            <Route
+              path="/calendar"
+              element={
+                <>
+                  <h1>Calendar</h1>
+                  <Calendar />
+                  <br />
+                  <SearchBar placeholder="Search for a study resource" data={resourcesData} />
+                  <br />
+                  <SearchBox options={options} onChange={newOptions => setOptions(newOptions)}/>
+                </>
+              }
+            />
+           
+            {/* Route for the timer/home page */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <h1>Pomodoro Timer</h1>
+                  {showSettings ? (
+                    <Settings setShowSettings={setShowSettings} />
+                  ) : (
+                    <Timer setShowSettings={setShowSettings} showSettings={showSettings} />
+                  )}
+                </>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </SettingsContext.Provider>
   );
 }
-
 export default App;
