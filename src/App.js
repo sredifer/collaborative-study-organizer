e49dc9184efa0fd8223d4bfd5b515e82601d3625
Lyra Latifi <lyralatifi@gmail.com>
2025-03-12 03:47:53 -0700

    Implement full search by date function
    Implement search by date function
    
    - rudimentary UI
    - fully implemented, searches articles in database by date uploaded

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import SearchBar from "./components/study-library-feature/study-library-feature"
 import resourcesData from "./components/Study-library-resources-data.json" 
 import SearchBox from "./components/tag-search-bar";
 import optionArray from "./components/constants/options";
+import DateSearchBar from "./components/search-by-date/search-by-date";
+import "./App.css"
 
 
 function App() {
   const [options, setOptions] = React.useState(optionArray);
 
   return (
-    <div>
+    <div className = "body">
       <h1>Collaborative Study Organizer</h1>
       <Calendar />
       <br />
@@ -18,6 +20,10 @@ function App() {
       <SearchBar placeholder="Search for a study resource" data={resourcesData}/>
       <br />
       <SearchBox options={options} onChange={newOptions => setOptions(newOptions)}/>
+      <br />
+      <div>
+      <DateSearchBar />
+      </div>
 
     </div>
   );
