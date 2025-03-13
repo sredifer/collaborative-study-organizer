5a932028c3dc202b9b7caeeabf680283021d33fd
Lyra Latifi <lyralatifi@gmail.com>
2025-03-12 17:47:34 -0700

    Finish Implementing Study Library Resources Database
    Finish Implementing study library resources database
    
    - articles now correct

--- src/components/search-by-date/search-by-date.js
+++ src/components/search-by-date/search-by-date.js
@@ -71,29 +71,76 @@ import "./search-by-date.css"
 // import "react-datepicker/dist/react-datepicker.css";
 
 const articles = [
-  {
-    "author": "Chinua Achebe",
-    "country": "Nigeria",
-    "imageLink": "images/things-fall-apart.jpg",
-    "language": "English",
-    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
-    "pages": 209,
-    "title": "Things Fall Apart",
-    "year": 2024,
-    "date": "02/14/2025"
-  },
-  {
-    "author": "Hans Christian Andersen",
-    "country": "Denmark",
-    "imageLink": "images/fairy-tales.jpg",
-    "language": "Danish",
-    "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
-    "pages": 784,
-    "title": "Fairy tales",
-    "year": 2025,
-    "date": "08/25/2025"
-  },
-  // Add more articles if needed
+    {
+    "title": "Best Study Tips for College",
+    "author": "Illinois Admissions",
+    "link": "https://blog.admissions.illinois.edu/best-study-tips-for-college/\n",
+    "date": "11/30/2022",
+    "tag": "Study Resource"
+    },
+    {
+     "title": "How to Study Effectively: 21 Best Study Tips for Final Exams",
+     "author": "Shorelight Team",
+     "link": "https://shorelight.com/student-stories/21-best-study-tips-for-final-exams/\n",
+     "date": "10/2/2024",
+     "tag": "Study Resource"
+    },
+    {
+     "title": "Top 10 Effective Study Tips for College Students",
+     "author": "Emily Hannigan-Page",
+     "link": "https://www.wgu.edu/blog/top-10-effective-study-tips-college-students2303.html\n",
+     "date": "03/14/2023",
+     "tag": "Study Resource"
+    },
+    {
+     "title": "8 Time Management Tips for Students",
+     "author": "Harvard Summer School: Division of Continuing Education",
+     "link": "https://summer.harvard.edu/blog/8-time-management-tips-for-students/\n",
+     "date": "11/21/2024",
+     "tag": "Time Management"
+    },
+    {
+     "title": "Time Management for Busy College Students",
+     "author": "Purdue Global",
+     "link": "https://www.purdueglobal.edu/blog/student-life/time-management-busy-college-students/\n",
+     "date": "02/05/2024",
+     "tag": "Time Management"
+    },
+    {
+     "title": "Time Management Strategies: 8 Tips for Balancing College and Life",
+     "author": "Krysten Godfrey Maddocks",
+     "link": "https://www.snhu.edu/about-us/newsroom/education/time-management-strategies\n",
+     "date": "05/24/2024",
+     "tag": "Time Management"
+    },
+    {
+     "title": "How to Be More Productive as a Student",
+     "author": "Matt Rowley",
+     "link": "https://www.columbiasouthern.edu/blog/blog-articles/2024/february/how-to-be-more-productive-as-a-student/\n",
+     "date": "02/14/2024",
+     "tag": "Productivity"
+    },
+    {
+     "title": "22 Productivity Tips for College Students",
+     "author": "Johnson and Wales University",
+     "link": "https://online.jwu.edu/blog/22-productivity-tips-college-students/\n",
+     "date": "10/20/2023",
+     "tag": "Productivity"
+    },
+    {
+     "title": "How To Study Effectively? 10 Best Study Techniques | USAHS",
+     "author": "University of St. Augustine for Health Sciences",
+     "link": "https://www.usa.edu/blog/study-techniques/#:~:text=Day%201:%20Learn%20the%20material,one%20week:%20Revisit%20and%20review.\n",
+     "date": "01/14/2025",
+     "tag": "Effective Learning"
+    },
+    {
+        "title": "Note-taking",
+        "author": "Erin Stapleton-Corcoran",
+        "link": "https://teaching.uic.edu/cate-teaching-guides/inclusive-equity-minded-teaching-practices/note-taking/\n",
+        "date": "10/30/2023",
+        "tag": "Effective Learning"
+    },
 ];
 
 function DateSearchBar() {
@@ -116,7 +163,7 @@ function DateSearchBar() {
 
     return (
         <div className="date-search-box">
-            <h2>Date Picker</h2>
+            <h1>Date Picker</h1>
             
             <DatePicker
                 selected={selectedDate}
@@ -132,7 +179,7 @@ function DateSearchBar() {
                             <li key={index}>
                                 <h3>{article.title}</h3>
                                 <p>Author: {article.author}</p>
-                                <p>Country: {article.country}</p>
+                                {/* <p>Country: {article.country}</p> */}
                                 <p>Published on: {article.date}</p>
                                 <p>Title: {article.title}</p>
                                 <a href={article.link} target="_blank" rel="noopener noreferrer">More Info</a>
